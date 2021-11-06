7ce3be65cdce33879fcc2b22f8d04d00be0e062a
foxster14 <sarah.fox.h@gmail.com>
2021-11-06 16:51:26 -0500

    adjusted feature names and made changes to gitignore

--- decision_tree.py
+++ decision_tree.py
@@ -1,8 +1,13 @@
-import pandas as pd
-from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
+ # Import Decision Tree Classifier
 from sklearn.model_selection import train_test_split # Import train_test_split function
 from sklearn import metrics # Import scikit-learn metrics module for accuracy calculation
 import xlsxwriter # Used to export data to Excel file
+import matplotlib as plt # Used to create visual representation of graphs and data structures
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+
+## look at how to keep the nodes uniform
 
 path = '/Users/sarahfox/OneDrive - Dunwoody College of Technology/Junior Year/Data Science/Decision Trees/' #path where all files are stored
 
@@ -16,15 +21,21 @@ data_frame = pd.read_csv(path + filename)
 x = data_frame[["Sex","BP","Cholesterol"]] # Features
 y = data_frame["Drug"] # Target variable
 
+titles = ["Sex","BP","Cholesterol","Drug"]
+
 # "leaves" (aka decision nodes) are where we get final output
 # root node is where the decision tree starts
 # Create Decision Tree classifer object
-tree = DecisionTreeClassifier(criterion="entropy")
+decision_tree = DecisionTreeClassifier(criterion="entropy", max_depth=3)
 one_hot_data = pd.get_dummies(x,drop_first=True)
 
 # Train Decision Tree Classifer
-tree = tree.fit(one_hot_data, y)
+decision_tree = decision_tree.fit(one_hot_data, y)
+
+text_representation = tree.export_text(decision_tree, feature_names=titles)
+print(text_representation)
+
 
 # Predict the response for test dataset
-##y_pred = tree.predict(one_hot_data)
+##y_pred = decision_tree.predict(one_hot_data)
 
